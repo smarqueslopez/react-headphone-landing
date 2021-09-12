a40a716d17385dc947555533234508523220c50a
smarqueslopez <smarqueslopez@gmail.com>
2021-09-12 17:48:59 +0200

    feat(global): :sparkles: Main components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
 import Head from 'next/head'
 import 'remixicon/fonts/remixicon.css'
-
+import Header from '../components/Header/Header'
+import Main from '../components/Main/Main'
+import Footer from '../components/Footer/Footer'
 export default function Home() {
   return (
-    <div>
+    <>
+      {/* METAPAGE INFO */}
       <Head>
-        <title>React Headphone Landing Page</title>
-        <meta name='description' content='React Headphone Landing Page' />
+        <title>React Responsive landing page headphones</title>
+        <meta
+          name='description'
+          content='React Responsive landing page headphones'
+        />
         <link rel='icon' href='/images/favicon.png' />
       </Head>
-    </div>
+      {/* PAGE */}
+      <>
+        {/* Header */}
+        <Header />
+        {/* Main */}
+        <Main />
+        {/* Footer */}
+        <Footer />
+      </>
+    </>
   )
 }
