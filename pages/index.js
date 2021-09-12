337511b579eed932d5bafeae2068b11d332985a9
smarqueslopez <smarqueslopez@gmail.com>
2021-09-12 17:11:34 +0200

    feat(global): :sparkles: Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import 'remixicon/fonts/remixicon.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>React Headphone Landing Page</title>
+        <meta name='description' content='React Headphone Landing Page' />
+        <link rel='icon' href='/images/favicon.png' />
+      </Head>
+    </div>
+  )
+}
