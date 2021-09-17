d9d138ea64eb39d9d6138ddedf68a8a3e0e1bd9a
smarqueslopez <smarqueslopez@gmail.com>
2021-09-17 14:48:21 +0200

    feat(global): :sparkles: Adding sections to main component

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -1,7 +1,22 @@
+import Home from './Home/Home'
+import SponsorList from './SponsorList/SponsorList'
+import Specs from './Specs/Specs'
+import Case from './Case/Case'
+import Discount from './Discount/Discount'
+import Products from './Products/Products'
 import style from './Main.module.scss'
 
 function Main() {
-  return <main className={style.main}></main>
+  return (
+    <main className={style.main}>
+      <Home />
+      <SponsorList />
+      <Specs />
+      <Case />
+      <Discount />
+      <Products />
+    </main>
+  )
 }
 
 export default Main
